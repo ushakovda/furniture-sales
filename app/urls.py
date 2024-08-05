49f6db40458b5f8e2263deebeab83a671f040204
ushakovda <ushakov328@gmail.com>
2024-08-05 18:55:19 +0700

    Configured display of discounts, category button

--- app/urls.py
+++ app/urls.py
@@ -17,17 +17,22 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
-from debug_toolbar.toolbar import debug_toolbar_urls
+from app import settings
 
-from app.settings import DEBUG
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls", namespace="main")),
     path("catalog/", include("goods.urls", namespace="catalog"))
-] + debug_toolbar_urls()
+]
 
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls'))
+    ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 """
 www.site.com/admin/
--- goods/views.py
+++ goods/views.py
@@ -1,83 +1,15 @@
 from django.shortcuts import render
 
+from goods.models import Products
+
 
 def catalog(request):
+
+    goods = Products.objects.all()
+
     context = {
         "title": "Home - Каталог",
-        "goods": [
-            {
-                "image": "deps/images/goods/set of tea table and three chairs.jpg",
-                "name": "Чайный столик и три стула",
-                "description": "Комплект из трёх стульев и дизайнерский столик для гостинной комнаты.",
-                "price": 150.00,
-            },
-            {
-                "image": "deps/images/goods/set of tea table and two chairs.jpg",
-                "name": "Чайный столик и два стула",
-                "description": "Набор из стола и двух стульев в минималистическом стиле.",
-                "price": 95.00,
-            },
-            {
-                "image": "deps/images/goods/double bed.jpg",
-                "name": "Двухспальная кровать",
-                "description": "Двухспальная кровать с изголовьем и ортопедическим матрасом в комплекте.",
-                "price": 680.00,
-            },
-            {
-                "image": "deps/images/goods/kitchen table.jpg",
-                "name": "Кухонный стол с раковиной",
-                "description": "Кухонный стол для обеда с встроенной раковиной и комплектом стильных стульев — идеальное решение для удобного и функционального обеденного пространства.",
-                "price": 465.00,
-            },
-            {
-                "image": "deps/images/goods/kitchen table 2.jpg",
-                "name": "Кухонный стол с встроенной плитой и раковиной",
-                "description": "Изысканный кухонный стол с встроенной плитой и раковиной, оснащённый множеством полок, чтобы каждый элемент вашей кухни был под рукой и выглядел безупречно.",
-                "price": 530.00,
-            },
-            {
-                "image": "deps/images/goods/corner sofa.jpg",
-                "name": "Угловой диван для гостинной",
-                "description": "Элегантный угловой диван, который раскладывается в удобную двухспальную кровать, идеально подойдёт для гостиной и удобного приёма гостей.",
-                "price": 720.00,
-            },
-            {
-                "image": "deps/images/goods/bedside table.jpg",
-                "name": "Прикроватный столик",
-                "description": "Прикроватный столик с двумя выдвижными ящиками (цветок не входит в комплект).",
-                "price": 55.00,
-            },
-            {
-                "image": "deps/images/goods/sofa.jpg",
-                "name": "Офисный диван",
-                "description": "Простой и элегантный офисный диван, который легко дополнит любой интерьер. Идеален для создания комфортабельной зоны отдыха или для неформальных встреч.",
-                "price": 190.00,
-            },
-            {
-                "image": "deps/images/goods/office chair.jpg",
-                "name": "Офисный стул",
-                "description": "Эргономичный офисный стул с простым дизайном, обеспечивающий комфорт и поддержку для вашего рабочего дня.",
-                "price": 30.00,
-            },
-            {
-                "image": "deps/images/goods/plants.jpg",
-                "name": "Цветок в горшке",
-                "description": "Цветок для украшения вашего интерьера подарит свежесть и безмятежность обстановке.",
-                "price": 10.00,
-            },
-            {
-                "image": "deps/images/goods/flower.jpg",
-                "name": "Цветок стилизированный",
-                "description": "Стильный дизайнерский цветок, который придаст вашему интерьеру утончённость и оригинальность. Отличный выбор для яркого декора.",
-                "price": 15.00,
-            },
-            {
-                "image": "deps/images/goods/strange table.jpg",
-                "name": "Прикроватный столик",
-                "description": "Классический прикроватный столик для комфортного доступа к вещам и добавления стиля в вашу спальню.",
-                "price": 35.00,
-            },
-        ],
+        "goods": goods,
     }
     return render(request, "goods/catalog.html", context)
 
