54addc19b45e431db8000e246521b94dab520598
ushakovda <ushakov328@gmail.com>
2024-08-18 19:45:31 +0700

    Registration form and controller are ready

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,15 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
 class UserLoginForm(AuthenticationForm):
 
+    class Meta:
+        model = User
+        fields = ['username',
+                  'password']
+        
     username = forms.CharField()
     password = forms.CharField()
 
@@ -21,7 +26,23 @@ class UserLoginForm(AuthenticationForm):
     #                                       'class': 'form-control',
     #                                       'placeholder': 'Введите ваш пароль'}),
     # )
+
+class UserRegistrationForm(UserCreationForm):
+
     class Meta:
         model = User
-        fields = ['username',
-                  'password']
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        )
+        
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
