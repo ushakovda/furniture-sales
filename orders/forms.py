153a8ff6aa2fe147f402a8d68d169d9cc2dfd0e8
ushakovda <ushakov328@gmail.com>
2024-08-23 13:40:44 +0700

    Create order is ready

--- orders/forms.py
+++ orders/forms.py
@@ -1,17 +1,25 @@
 from django import forms
 
+
 class CreateOrderForm(forms.Form):
-    
+
     first_name = forms.CharField()
     last_name = forms.CharField()
     phone_number = forms.CharField()
-    requires_delivery = forms.ChoiceField()
-    delivery_address = forms.ChoiceField(required=False)
-    payment_on_get = forms.CharField()
+    requires_delivery = forms.ChoiceField(
+        choices=[
+            ("0", False),
+            ("1", True),
+        ],
+    )
+    delivery_address = forms.CharField(required=False)
+    payment_on_get = forms.ChoiceField(
+        choices=[
+            ("0", False),
+            ("1", True),
+        ],
+    )
 
-
-
-    
     # first_name = forms.CharField(
     #     widget=forms.TextInput(
     #         attrs={
--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from carts.admin import CartTabAdmin
+from orders.admin import OrderTabulareAdmin
 from users.models import User
 
 # admin.site.register(User)
@@ -11,5 +12,5 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ['username', 'first_name', 'last_name', 'email',]
     
 
-    inlines = [CartTabAdmin,]
+    inlines = [CartTabAdmin, OrderTabulareAdmin]
 
